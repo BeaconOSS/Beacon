2076fbce91f35676c8645e410384357b29a49f54
Thijs Hankel <72493994+ThijsHankelMC@users.noreply.github.com>
2026-06-15 22:08:30 +0200

    feat: init login

--- frontend/app/components/AppHeader.vue
+++ frontend/app/components/AppHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="header">
     <span class="logo">Beacon</span>
-    <button class="sign-in" type="button">Sign in</button>
+    <NuxtLink class="sign-in" to="/login">Sign in</NuxtLink>
   </header>
 </template>
---
+++ frontend/app/pages/login.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="login">
+    <h1>Sign in</h1>
+    <form class="login-form" @submit.prevent>
+      <label class="field">
+        <span>Email</span>
+        <input type="email" name="email" autocomplete="email" />
+      </label>
+      <label class="field">
+        <span>Password</span>
+        <input type="password" name="password" autocomplete="current-password" />
+      </label>
+      <button class="submit" type="submit">Sign in</button>
+    </form>
+  </section>
+</template>
