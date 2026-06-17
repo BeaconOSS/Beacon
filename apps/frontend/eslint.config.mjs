cc5cbd46cc88e7d02d1c60a3f48a9f5f2947532d
Thijs Hankel <72493994+ThijsHankelMC@users.noreply.github.com>
2026-06-17 22:19:32 +0200

    Update eslint.config.mjs

--- apps/frontend/eslint.config.mjs
+++ apps/frontend/eslint.config.mjs
@@ -5,6 +5,7 @@ export default withNuxt({
 	rules: {
 		"vue/max-attributes-per-line": "off",
 		"vue/singleline-html-element-content-newline": "off",
+		"vue/html-self-closing": "off",
 		"import/consistent-type-specifier-style": ["error", "prefer-top-level"],
 		"import/newline-after-import": "error",
 		"import/order": [
